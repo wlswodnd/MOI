fc3a42f2fb7c072db00b86fd136d1a789ca46092
wlswodnd <weer41031@gmail.com>
2021-08-22 17:50:52 +0900

    1

--- index/views.py
+++ index/views.py
@@ -4,23 +4,26 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-def login( request ) :
-    return render( request, "login.html")
+# def login( request ) :
+#     return render( request, "login.html")
+#
+# def join( request ) :
+#     return render( request, "join.html")
 
-def join( request ) :
-    return render( request, "join.html")
+def information( request ) :
+    return render( request, "information.html")
 
-def search( request ) :
-    return render( request, "search.html")
+# def 게시판( request ) :
+#     return render( request, "게시판.html")
+#
+# def 상세페이지( request ) :
+#     return render( request, "상세페이지.html")
+#
+# def 수정( request ) :
+#     return render( request, "수정.html")
+#
+# def 글쓰기( request ) :
+#     return render( request, "글쓰기.html")
 
-def 게시판( request ) :
-    return render( request, "게시판.html")
-
-def 상세페이지( request ) :
-    return render( request, "상세페이지.html")
-
-def 수정( request ) :
-    return render( request, "수정.html")
-
-def 글쓰기( request ) :
-    return render( request, "글쓰기.html")
+def Introduce( request ) :
+    return render( request, "Introduce.html")
