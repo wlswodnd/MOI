20d1c04c0fd89b10998ccd60b7687fd3148044e6
kyeonghye <tjghdwn0824@naver.com>
2021-10-23 16:46:17 +0900

    마지막

--- index/views.py
+++ index/views.py
@@ -23,3 +23,33 @@ def Gyeongju( request ) :
 
 def map( request ) :
     return render( request, "map.html")
+
+def Daegu( request ) :
+    return render( request, "Daegu.html")
+
+def Gangneung( request ) :
+    return render( request, "Gangneung.html")
+
+def Sokcho( request ) :
+    return render( request, "Sokcho.html")
+
+def Cheonan( request ) :
+    return render( request, "Cheonan.html")
+
+def Boryeong( request ) :
+    return render( request, "Boryeong.html")
+
+def Gwangju( request ) :
+    return render( request, "Gwangju.html")
+
+def Incheon( request ) :
+    return render( request, "Incheon.html")
+
+def Mokpo( request ) :
+    return render( request, "Mokpo.html")
+
+def ChunCheon( request ) :
+    return render( request, "ChunCheon.html")
+
+def Wonju( request ) :
+    return render( request, "Wonju.html")
